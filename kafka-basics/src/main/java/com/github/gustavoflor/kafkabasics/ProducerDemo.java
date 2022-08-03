76dc4787ab3e72a45e0afa361ce56754e9540d82
gustavo-flor <gustavo.flor@picpay.com>
2022-08-02 23:10:58 -0300

    :tada: First commit

---
+++ kafka-basics/src/main/java/com/github/gustavoflor/kafkabasics/ProducerDemo.java
@@ -0,0 +1,33 @@
+package com.github.gustavoflor.kafkabasics;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
+    public static void main(String[] args) {
+        log.info("Hello World!");
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("java-basics", "Hello World");
+
+        kafkaProducer.send(producerRecord);
+
+        kafkaProducer.flush();
+
+        kafkaProducer.close();
+    }
+}
